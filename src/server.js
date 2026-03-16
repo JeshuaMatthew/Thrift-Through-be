2695d10f0311eb8b3bfdeb934324dde64cde5be0
jeshuaMatthew <2381009@unai.edu>
2026-03-16 22:27:00 +0700

    fix: cors fix

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const { Server } = require('socket.io');
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://thrift-through-fe.vercel.app'],
         credentials: true
     }
 });
@@ -28,7 +28,7 @@ const chatSocket = require('./socket/chatSocket');
 chatSocket(io);
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173', 'https://thrift-through-fe.vercel.app'], 
     credentials: true 
 }));
 
