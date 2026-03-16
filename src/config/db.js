fb5f58b8a31338260da2b0dca8fbabf8f613cb52
jeshuaMatthew <2381009@unai.edu>
2026-03-16 22:49:52 +0700

    fix: changed the port again

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.DB_HOST,
-    port: 8080,
+    port: parseInt(process.env.DB_PORT || '5432'),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
